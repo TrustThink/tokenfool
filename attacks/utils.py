d70e2e5f770563cb5d99635c613fb8cfd3e7a7e8
Allison Lane <lane_allison@trustthink.net>
2026-02-27 12:43:56 -0800

    add utils, update reqs

---
+++ attacks/utils.py
@@ -0,0 +1,14 @@
+import torch
+
+def clamp(X, lower_limit, upper_limit):
+    return torch.max(torch.min(X, upper_limit), lower_limit)
+
+def PCGrad(atten_grad, ce_grad, sim, shape):
+    pcgrad = atten_grad[sim < 0]
+    temp_ce_grad = ce_grad[sim < 0]
+    dot_prod = torch.mul(pcgrad, temp_ce_grad).sum(dim=-1)
+    dot_prod = dot_prod / torch.norm(temp_ce_grad, dim=-1)
+    pcgrad = pcgrad - dot_prod.view(-1, 1) * temp_ce_grad
+    atten_grad[sim < 0] = pcgrad
+    atten_grad = atten_grad.view(shape)
+    return atten_grad
