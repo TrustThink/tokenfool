dde74c3c785d48c230df57aca03163eec25adc60
Allison Lane <lane_allison@trustthink.net>
2026-03-16 09:38:21 -0700

    export public adapter & attack APIs

---
+++ tokenfool/adapters/__init__.py
@@ -0,0 +1,4 @@
+from .interfaces import TransformerClassifier, HookableTransformerClassifier
+from .timm_vit import TimmViTAdapter
+
+__all__ = ["TransformerClassifier", "HookableTransformerClassifier", "TimmViTAdapter"]
---
+++ tokenfool/attacks/__init__.py
@@ -0,0 +1,4 @@
+from .att import ATT
+from .patchfool import PatchFool
+
+__all__ = ["ATT", "PatchFool"]
