e5695596e814f84fdcc74de0c0e0192c38f37348
Allison Lane <lane_allison@trustthink.net>
2026-03-30 15:50:53 -0700

    add Hugging Face ViT Adapter & example notebook

--- tokenfool/adapters/__init__.py
+++ tokenfool/adapters/__init__.py
@@ -1,4 +1,5 @@
 from .interfaces import TransformerClassifier, HookableTransformerClassifier
 from .timm_vit import TimmViTAdapter
+from .hf_vit import HuggingFaceViTAdapter
 
-__all__ = ["TransformerClassifier", "HookableTransformerClassifier", "TimmViTAdapter"]
+__all__ = ["TransformerClassifier", "HookableTransformerClassifier", "TimmViTAdapter", "HuggingFaceViTAdapter"]
